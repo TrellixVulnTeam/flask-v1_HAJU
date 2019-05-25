db847f954c8f14fffc560fd1f5d6a46097a212f6
pafable <philafable@gmail.com>
2019-05-25 07:26:16 -0400

    added error handling

--- code/app.py
+++ code/app.py
@@ -8,12 +8,13 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item is not None else 404
 
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {"message": "An item with name '{}' already exists.".format(name)}, 400
+
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
